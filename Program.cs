1b2d5f4bce56302a05839f9652c07f0c5b76bff5
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-05-31 04:50:18 -0300

    instancie la lista tareas pendientes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿Random rand = new Random();
+﻿using tp07_2022_exequiel1984;
 
+string descripcion ="";
+int duracion;
+
+
+Random rand = new Random();
+List<tarea> tareasPendientes = new List<tarea>();
+
+
+Console.WriteLine("Ingrese la cantidad de tareas a cargar: ");
+int cantidadTareas = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <=  cantidadTareas; i++)
+{
+    tarea tarea = new tarea();
+    Console.WriteLine("Ingrese la descripcion: ");
+    descripcion = Console.ReadLine();
+    duracion = rand.Next(1, 61);
+    tarea.cargarTarea(i, descripcion, duracion);
+}
+
+
+
+foreach (var tarea in tareasPendientes)
+{
+    tarea.mostrarTarea();
+}
