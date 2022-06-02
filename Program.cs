e08c2076a631d701892e415e71416b15c43bbaae
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-06-02 14:49:22 -0300

    creo lista de tareas pendientes

--- Program.cs
+++ Program.cs
@@ -2,24 +2,41 @@
 
 string descripcion;
 int duracion;
-
-
+string salida = "";
+int IdTarea = 1;
+string DescripcionTarea;
 Random rand = new Random();
 List<tarea> tareasPendientes = new List<tarea>();
 
 
-Console.WriteLine("Ingrese la cantidad de tareas a cargar: ");
-int cantidadTareas = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ingrese la cantidad de tareas a cargar: ");
+// int cantidadTareas = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i <= cantidadTareas; i++)
+// for (int i = 0; i <= cantidadTareas; i++)
+// {
+//     tarea tarea = new tarea();
+//     Console.WriteLine("Ingrese la descripcion: ");
+//     descripcion = Console.ReadLine();
+//     duracion = rand.Next(1, 61);
+//     tarea.cargarTarea(i, descripcion, duracion);
+// }
+
+do
 {
-    tarea tarea = new tarea();
-    Console.WriteLine("Ingrese la descripcion: ");
-    descripcion = Console.ReadLine();
-    duracion = rand.Next(1, 61);
-    tarea.cargarTarea(i, descripcion, duracion);
-}
+    System.Console.WriteLine("Ingrese la descripcion de la tarea:");
+    DescripcionTarea = Console.ReadLine();
 
+    tarea Ntarea = new tarea();
+
+    Ntarea.TareaID = IdTarea++;
+    Ntarea.Descripcion = DescripcionTarea;
+    duracion = rand.Next(1, 61);
+
+    tareasPendientes.Add(Ntarea);
+
+    System.Console.WriteLine("Desea ingresar una nueva tarea (s/n)?");
+    salida = Console.ReadLine();
+} while (salida != "n");
 
 
 foreach (var tarea in tareasPendientes)
