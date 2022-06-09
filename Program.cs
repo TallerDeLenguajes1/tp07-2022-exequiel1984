2ed8a8c2ef6f7943ca88b98a73b9a6a386c2a549
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-06-09 14:06:43 -0300

    no puedo eliminar la tarea pendiente que se realiz

--- Program.cs
+++ Program.cs
@@ -5,41 +5,58 @@ int duracion;
 string salida = "";
 int IdTarea = 1;
 string DescripcionTarea;
+string ControlTareaRealizada = "";
+
 Random rand = new Random();
-List<tarea> tareasPendientes = new List<tarea>();
-
-
-// Console.WriteLine("Ingrese la cantidad de tareas a cargar: ");
-// int cantidadTareas = Convert.ToInt32(Console.ReadLine());
-
-// for (int i = 0; i <= cantidadTareas; i++)
-// {
-//     tarea tarea = new tarea();
-//     Console.WriteLine("Ingrese la descripcion: ");
-//     descripcion = Console.ReadLine();
-//     duracion = rand.Next(1, 61);
-//     tarea.cargarTarea(i, descripcion, duracion);
-// }
+List<tarea> ListadotareasPendientes = new List<tarea>();
+List<tarea> ListadotareasRealizadas = new List<tarea>();
 
 do
 {
     System.Console.WriteLine("Ingrese la descripcion de la tarea:");
     DescripcionTarea = Console.ReadLine();
 
-    tarea Ntarea = new tarea();
+    tarea NuevaTareaPendiente = new tarea();
 
-    Ntarea.TareaID = IdTarea++;
-    Ntarea.Descripcion = DescripcionTarea;
-    Ntarea.Duracion = rand.Next(1, 61);
+    NuevaTareaPendiente.TareaID = IdTarea++;
+    NuevaTareaPendiente.Descripcion = DescripcionTarea;
+    NuevaTareaPendiente.Duracion = rand.Next(1, 61);
 
-    tareasPendientes.Add(Ntarea);
+    ListadotareasPendientes.Add(NuevaTareaPendiente);
 
     System.Console.WriteLine("Desea ingresar una nueva tarea (s/n)?");
     salida = Console.ReadLine();
 } while (salida != "n");
 
-System.Console.WriteLine("\nListado de tareas cargadas\n");
-foreach (var tarea in tareasPendientes)
+System.Console.WriteLine("\n-----Listado de tareas pendientes cargadas-----\n");
+foreach (var TareaPendiente in ListadotareasPendientes)
 {
-    tarea.mostrarTarea();
+    TareaPendiente.mostrarTarea();
+}
+
+System.Console.WriteLine("\n-----Control de tareas realizadas-----\n");
+foreach (var tareaPendiente in ListadotareasPendientes)
+{
+    tareaPendiente.mostrarTarea();
+    System.Console.WriteLine("La tarea se realizo (s/n)?");
+    ControlTareaRealizada = Console.ReadLine();
+    if (ControlTareaRealizada == "s")
+    {
+        tarea NuevaTareaRealizada = new tarea();
+        NuevaTareaRealizada = tareaPendiente;
+        ListadotareasRealizadas.Add(NuevaTareaRealizada);
+        ListadotareasPendientes.Remove(tareaPendiente));
+    }
+}
+
+System.Console.WriteLine("\n-----Listado de tareas pendientes-----\n");
+foreach (var TareaPendiente in ListadotareasPendientes)
+{
+    TareaPendiente.mostrarTarea();
+}
+
+System.Console.WriteLine("\n-----Listado de tareas realizadas-----\n");
+foreach (var TareaRealizada in ListadotareasRealizadas)
+{
+    TareaRealizada.mostrarTarea();
 }
